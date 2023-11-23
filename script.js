3e8687f79cdf11ea80823654ac800d48186cdf97
Abhinav Mathur <142848301+Abhinav290397@users.noreply.github.com>
2023-11-24 03:12:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+function generateRandomNum(){
+	let randomNumber = parseInt(Math.random()*130);
+	if(randomNumber < 47){
+		randomNumber += 47;
+	}
+	return randomNumber;
+}
 function makeid(l) {
   // write your code here
+	let str = "";
+	for(let i=0;i<l;i++){
+		str += String.fromCharCode(generateRandomNum);
+	}
+	return str;
 }
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
+/*const l = prompt("Enter a number.");
+alert(makeid(l));*/
+
